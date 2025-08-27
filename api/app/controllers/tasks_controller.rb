be85c4dae9a8db0b838805240004700e40459f20
mateus cristian <mateus_cristian@outlook.com>
2025-08-27 20:51:58 -0300

    feat: criação controller,model,service referente a Task

---
+++ api/app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update]
+
+  def index
+    tasks = Task.all
+    render json: tasks.map(&:as_json)
+  end
+
+  def show
+    render json: @task.as_json
+  end
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: task.as_json, status: :created
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task.as_json
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :due_date, :active)
+  end
+end
---
+++ api/app/models/task.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Task < ApplicationRecord
+
+    validates :name, presence: true
+    validates :description, presence: true
+
+    def as_json(_options = {})
+    {
+      id: id,
+      name: name,
+      description: description,
+      due_date: due_date,
+      active: active,
+    }
+  end
+end
