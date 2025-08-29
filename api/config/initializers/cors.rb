32ff16e5f596c2d7dd0e3b049dd002831cf04988
mateus cristian <mateus_cristian@outlook.com>
2025-08-29 08:31:18 -0300

    feat: config devise para autenticacao

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # Em produção, coloque o domínio do seu frontend
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
+  end
+end
