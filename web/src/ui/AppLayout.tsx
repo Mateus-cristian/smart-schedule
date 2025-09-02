38b891d7b24c3b586bc8bb049272043afac123b4
mateus cristian <mateus_cristian@outlook.com>
2025-09-01 22:29:38 -0300

    feat: Adição componente Loader

--- web/src/ui/AppLayout.tsx
+++ web/src/ui/AppLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
 import { type JSX } from "react";
-import GlobalLoader from "@/components/GlobalLoader";
+import Loader from "@/components/Loader";
 import { Toaster } from "react-hot-toast";
 
 function AppLayout(): JSX.Element {
   return (
     <div className="min-h-screen bg-gray-50">
-      <GlobalLoader />
+      <Loader />
       <Toaster />
       <Outlet />
     </div>
