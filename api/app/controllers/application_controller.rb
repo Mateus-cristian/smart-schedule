a8193875a3206be227baec31579751fc43a0377f
mateus cristian <mateus_cristian@outlook.com>
2025-09-02 19:57:10 -0300

    refactor: autenticação

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!, unless: :devise_controller?
+  include ActionController::Cookies
+  before_action :authenticate_with_jwt!
 
-  def current_user_json
-    if user_signed_in?
-      render json: { user: current_user.slice(:id, :email) }
-    else
-      render json: { user: nil }, status: :unauthorized
+  private
+
+  def authenticate_with_jwt!
+    token = cookies.encrypted[:access_token]
+    if token.present?
+      begin
+        payload = Warden::JWTAuth::TokenDecoder.new.call(token)
+        @current_user = User.find_by(id: payload["sub"])
+      rescue
+        @current_user = nil
+      end
     end
+
+    unless @current_user
+      render json: { error: "You need to sign in or sign up before continuing." }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
   end
 end
--- api/app/controllers/users/sessions_controller.rb
+++ api/app/controllers/users/sessions_controller.rb
@@ -1,25 +1,35 @@
 # frozen_string_literal: true
-
 module Users
-  class Users::SessionsController < Devise::SessionsController
-    respond_to :json
+  class SessionsController < Devise::SessionsController
+      respond_to :json
+      skip_before_action :authenticate_with_jwt!, only: [:create ]
 
-    def show
-      if current_user
-        render json: { user: current_user }, status: :ok
-      else
-        render json: { user: nil }, status: :unauthorized
+      def show
+        if current_user
+          render json: { user: current_user }, status: :ok
+        else
+          render json: { user: nil }, status: :unauthorized
+        end
       end
-    end
 
-    private
+      private
 
-    def respond_with(resource, _opts = {})
-      render json: { user: resource }, status: :ok
-    end
+       def respond_with(resource, _opts = {})
+        token = request.env['warden-jwt_auth.token']
+        cookies.encrypted[:access_token] = {
+          value: token,
+          httponly: true,
+          secure: Rails.env.production?,
+          same_site: :lax,
+          expires: 1.day.from_now
+        }
+        render json: { user: resource }, status: :ok
+      end
 
-    def respond_to_on_destroy
-      head :no_content
-    end
+      def respond_to_on_destroy
+        cookies.delete(:access_token, secure: Rails.env.production?, same_site: :lax)
+        head :no_content
+      end
   end
 end
+
