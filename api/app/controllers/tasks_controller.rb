7d0ed8ef2346bc0bfa7f1ca31765381de3acb78b
mateus cristian <mateus_cristian@outlook.com>
2025-09-03 20:38:04 -0300

    feat: criado rota e função para excluir uma tarefa

--- api/app/controllers/tasks_controller.rb
+++ api/app/controllers/tasks_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :set_task, only: [ :show, :update ]
-
+  before_action :set_task, only: [ :show, :update, :destroy ]
+  
   def index
     tasks = Task.all
     render json: tasks, each_serializer: TaskSerializer
   end
-
+  
   def show
     render json: @task, serializer: TaskSerializer
   end
-
+  
   def create
     task = Task.new(task_params)
     if task.save
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
       render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
     end
   end
-
+  
   def update
     if @task.update(task_params)
       render json: @task, serializer: TaskSerializer
@@ -28,14 +28,22 @@ class TasksController < ApplicationController
       render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
     end
   end
-
+  
+  def destroy
+    if @task.destroy
+      head :no_content
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
   private
-
+  
   def set_task
     @task = Task.find(params[:id])
   end
 
   def task_params
-    params.require(:task).permit(:name, :description, :due_date, :active)
+    params.require(:task).permit(:title, :description, :due_date, :active)
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   end
 
 
-  resources :tasks, only: [:create, :show, :update, :index]
+  resources :tasks, only: [:create, :show, :update, :index, :destroy]
 end
