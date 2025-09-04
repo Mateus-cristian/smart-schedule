a0f1defdccf52761174b487ab3462dc4e5f5edfe
mateus cristian <mateus_cristian@outlook.com>
2025-09-03 21:06:18 -0300

    feat: adicionado propriedade active no TaskSerializer

--- api/app/serializers/task_serializer.rb
+++ api/app/serializers/task_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TaskSerializer < ActiveModel::Serializer
-	attributes :id, :title, :description, :due_date
+	attributes :id, :title, :description, :due_date, :active
 
 	def due_date
 		object.due_date&.strftime("%d/%m/%Y")
