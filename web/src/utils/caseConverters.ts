464d62a2ffc367ebd8677bed4a944156fa350dfa
mateus cristian <mateus_cristian@outlook.com>
2025-09-07 15:01:39 -0300

    feat função para ajuste profundo em objetos de snake case para camel case

--- web/src/utils/caseConverters.ts
+++ web/src/utils/caseConverters.ts
@@ -1,5 +1,16 @@
 import { mapKeys, isArray, isObject, camelCase, snakeCase } from "lodash";
 
+export function deepToCamelCase(obj: any): any {
+  if (isArray(obj)) return obj.map(deepToCamelCase);
+  if (isObject(obj)) {
+    return Object.entries(obj).reduce((acc, [k, v]) => {
+      acc[camelCase(k)] = deepToCamelCase(v);
+      return acc;
+    }, {} as any);
+  }
+  return obj;
+}
+
 export function toCamelCase(obj: any): any {
   if (isArray(obj)) return obj.map(toCamelCase);
   if (isObject(obj)) {
